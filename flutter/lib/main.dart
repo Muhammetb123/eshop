4f5acc1609af4038e0ef1b5a36d79e3f287b10cd
Muhammetb123 <muhammetb1997@gmail.com>
2025-06-18 03:34:36 -0600

    home page baslanan

--- flutter/lib/main.dart
+++ flutter/lib/main.dart
@@ -1,1 +1,24 @@
-void main()  {}
+import 'package:e_commerce/view/homePageContent.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(EShop());
+}
+
+class EShop extends StatelessWidget {
+  const EShop({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // darkTheme: ThemeData.dark(),
+      home: HomePageContent(),
+      theme: ThemeData(
+        appBarTheme: AppBarTheme(backgroundColor: Colors.green),
+        bottomNavigationBarTheme: BottomNavigationBarThemeData(
+          backgroundColor: Colors.white,
+        ),
+      ),
+    );
+  }
+}
